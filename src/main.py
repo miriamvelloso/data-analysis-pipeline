f14d16826be21699d2cf9f0c944c91115aa9c614
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-06-21 20:29:51 +0200

    main

--- src/main.py
+++ src/main.py
@@ -7,46 +7,28 @@ import seaborn as sns
 
 
 def parse():
-    parser=ArgumentParser(description="Este programa es para buscar los streams de los artistas cuyas canciones son las más reproducidas en Spotify en el año 2019")
-    parser.add_argument("--artist",dest="Artist",help="El artista de la canción")
-    parser.add_argument("--songname",dest="song_name",help="El nombre del single")
+    parser=ArgumentParser(description="Este programa es para buscar los 10 mejores cantantes para cada popularidad y streamings seleccionado")
+    parser.add_argument("--followers",dest="followers",help="filtras según el número de followers con respecto al resto de artistas. opciones disponibles('very low', 'low', 'medium', 'high')")
+    parser.add_argument("--streams",dest="streams",help="filtras según el número de streams con respecto al resto de artistas.Opciones disponibles('very low', 'low', 'medium', 'high')")
     return parser.parse_args()
 
 
 def main():
     args=parse()
-    Artist=args.artist
-    song_name=args.song_name
-    print(artist)
-    print(songname)
+    Artist=args.followers
+    song_name=args.streams
+    print(followers)
+    print(streams)
     data=acquire()
-    filtered = filters(data,Artist,song_name)
+    filtered = filters(data,followers,streams)
     results = analysis(filtered)
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-def main():
-    args=parse()
-    year=args.year
-    tour=args.tour
-    print(year)
-    print(tour)
-    data=acquire()
-    filtered = filters(data,year,tour)
-    results = analysis(filtered)
-    barchart = visualize(results,tour,year)
+    barchart = visualize(results,followers,streams)
     save_chart(barchart)
-    filtered2 = filters2(data)
-    results2 = analysis2(filtered2)
-    winners_data = acquire2()
-    send_email(results2,winners_data)
-    
+
+
 if __name__ == '__main__':
     main()
+
 
 
 
