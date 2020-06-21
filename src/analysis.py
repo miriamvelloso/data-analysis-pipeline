5516311cbe1c04a1db774aebc20d9b7d260855a4
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-06-21 19:34:51 +0200

    añadiendocambios

--- src/analysis.py
+++ src/analysis.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 def acquire():
-    data=pd.read_csv('input/spotify_clean.csv')
+    data=pd.read_csv('spotify_clean.csv')
     return data
 
-def filters(df,song_name,streams):
-    filtered=df[(df["song_name"]==song)&(df["Streams"]==streams)]
+def filters(df,Artist_follower,Streams1):
+    filtered=df[(df["Artist_follower"]==followers)&(df["Streams1"]==streams)]
     print(filtered)
     return filtered
 
 def analysis(df):
-    grouped=df.groupby("song").agg({"Streams":"sum"}).reset_index()
-    results=grouped.sort_values("Streams",ascending=False).head(10)
+    grouped=df.groupby("Artist").agg({"Artist_popularity":"mean"}).reset_index()
+    results=grouped.sort_values("Artist_popularity",ascending=False).head(10)
     print(results)
     return results
--- src/charts.py
+++ src/charts.py
@@ -4,14 +4,15 @@ import seaborn as sns
 from analysis import *
 
 
-def visualize(df,song_name,streams):
-    title = 'Top 10  '+ song_name + ' with most streams'
+def visualize(df,Artist,Artist_popularity):
+    title = 'Top 10 most popularity Artists '
     fig, ax = plt.subplots(figsize=(15,8))
-    barchart = sns.barplot(data=df, x='Srtist', y='Streams')
+    barchart = sns.barplot(data=df, x='Artist', y='Artist_popularity')
     plt.title(title + "\n", fontsize=10)
     return barchart
 
 def save_chart(graph):
-    title2 = 'Top 10 songs 2019'
+    title2 = 'Top 10 most popularity Artists'
     fig=graph.get_figure()
     fig.savefig(title2 + '.png')
+
